5ba926cf9e7a66df4985b3a6329d9f53e0e39fda
jordanlafferty <78756047+jordanlafferty@users.noreply.github.com>
2022-10-11 09:14:30 -0500

    Updated for HW 12

--- Parser_Parser.rkt
+++ Parser_Parser.rkt
@@ -1,95 +1,63 @@
 #lang racket
-
 (require "Parser_Utility.rkt")
-(define resolve
-  (lambda (environment varname)
+(define neo-parser
+  (lambda (neo-code)
     (cond
-      ((null? environment) #false)
-      ((equal? (caar environment) varname) (cadar environment))
-      (else (resolve (cdr environment) varname))
+      ((null? neo-code) '())
+      ((number? neo-code) (list 'num-exp neo-code))
+      ((symbol? neo-code) (list 'var-exp neo-code))
+      ;(bool op num1 num2) > (bool-exp op (neo-exp) (neo-exp))
+       ((equal? (car neo-code) 'bool) (neo-bool-code-parser neo-code))
+      ;(math op num1 num2) > (math-exp op (neo-exp) (neo-exp))
+      ((equal? (car neo-code) 'math) (neo-math-code-parser neo-code))
+      ;(ask (bool op num1 num2) (neo-exp1) (neo-exp2)) > (ask-exp (bool-exp ...) (parsed-neo-exp1) (parsed-neo-exp2))
+      ((equal? (car neo-code) 'ask) (neo-ask-code-parser neo-code))
+      ((equal? (car neo-code) 'function) (neo-function-code-parser neo-code))
+      ;(call (function (x y z) (math + (math + x y) z)) (1 2 3)) ->
+      ;(app-exp (func-exp (params (identifier1, identifier2, identifer3 ...)) (body-exp)) ((neo-exp1 neo-exp2 neo-exp3 ...))
+      ((equal? (car neo-code) 'call) (neo-call-code-parser neo-code))
+      (else (map neo-parser neo-code)) ;((neo-parser 1) (neo-parser 'a) (neo-parser (math + 1 2)))
       )
     )
   )
 
-(define extend-env
-  (lambda (list-of-varname list-of-value env) ;((x y z) (1 2 3) env)
-    (cond
-      ((null? list-of-varname) env)
-      ((null? list-of-value) env)
-      (else (extend-env (cdr list-of-varname) (cdr list-of-value)
-       (cons (list (car list-of-varname)
-                   (car list-of-value))
-             env)))
-      )
+;a parser only for boolean expression
+(define neo-bool-code-parser
+  (lambda (neo-code)
+     (if (equal? (length neo-code) 3)
+            (list 'bool-exp (elementAt neo-code 1) (neo-parser (caddr neo-code)) '())
+        (cons 'bool-exp (cons (cadr neo-code) (map neo-parser (cddr neo-code)))))     
     )
   )
 
-(define run-neo-parsed-code
-  (lambda (parsed-code env)
-    (cond
-      ((null? parsed-code) '())
-      ((equal? (car parsed-code) 'num-exp)
-       (cadr parsed-code));(num-exp 22)
-      ((equal? (car parsed-code) 'var-exp)
-       (resolve env (cadr parsed-code)))
-      ;(bool-exp op (neo-exp) (neo-exp))
-      ((equal? (car parsed-code) 'bool-exp) (run-bool-parsed-code (cdr parsed-code) env))
-      ;(math-exp op (neo-exp) (neo-exp))
-      ((equal? (car parsed-code) 'math-exp)
-       (run-math-exp (cadr parsed-code)
-                     (run-neo-parsed-code (caddr parsed-code) env)
-                     (run-neo-parsed-code (cadddr parsed-code) env)))
-      ((equal? (car parsed-code) 'ask-exp)
-       (if (run-neo-parsed-code (cadr parsed-code) env)
-           (run-neo-parsed-code (caddr parsed-code) env)
-           (run-neo-parsed-code (cadddr parsed-code) env)))
-      ((equal? (car parsed-code) 'func-exp)
-       (run-neo-parsed-code (cadr (caddr parsed-code)) env))
-      (else (run-neo-parsed-code
-             (cadr parsed-code) ;function expression
-             (extend-env
-              (cadr (cadr (cadr parsed-code)))
-              (map (lambda (exp) (run-neo-parsed-code exp env)) (caddr parsed-code));list of values ((num-exp 1) (var-exp a) (math-exp + (num-exp 2) (num-exp 3)))
-              env);environment scope update
-             )
-            )
-      )
-    ) 
-  )
-
-
-;run bool parsed code
-(define run-bool-parsed-code
-  (lambda(parsed-code env)
-    (let ((op (elementAt parsed-code 0))
-           (num1 (run-neo-parsed-code (elementAt parsed-code 1) env))
-           (num2 (run-neo-parsed-code (elementAt parsed-code 2) env)))
-           (cond
-             ((equal? op '>) (> num1 num2))
-             ((equal? op '<) (< num1 num2))
-             ((equal? op '>=) (>= num1 num2))
-             ((equal? op '<=) (<= num1 num2))
-             ((equal? op '==) (= num1 num2))
-             ((equal? op '!=) (not (= num1 num2)))
-             ((equal? op '&&) (and num1 num2))
-             ((equal? op '||) (or num1 num2))
-             (else (not num1))
-        )
-      )
+(define neo-math-code-parser
+  (lambda (neo-code)
+    (list 'math-exp (cadr neo-code)
+             (neo-parser (caddr neo-code))
+             (neo-parser (cadddr neo-code)))
     )
   )
 
-(define run-math-exp
-  (lambda (op num1 num2)
-    (cond
-      ((equal? op '+) (+ num1 num2)) ;+ 1 1)
-      ((equal? op '-) (- num1 num2))  ;- 1 1
-      ((equal? op '*) (* num1 num2)) ;* 1 1
-      ((equal? op '/) (/ num1 num2))  ;/ 1 1 float division
-      ((equal? op '//) (quotient num1 num2))  ;// 1 1 interger division
-      ((equal? op '%) (modulo num1 num2))  ;% 1 1 modulo
-      (else #false)
-      )
+(define neo-function-code-parser
+  (lambda (neo-code)
+    (list 'func-exp
+             (list 'params (cadr neo-code))
+             (list 'body-exp (neo-parser (caddr neo-code))))
+    )
+  )
+
+(define neo-ask-code-parser
+  (lambda (neo-code)
+    (cons 'ask-exp
+             (map neo-parser (cdr neo-code)))
+    )
+  )
+
+(define neo-call-code-parser
+  (lambda(neo-code)
+    (list 'app-exp
+             (neo-parser (cadr neo-code))
+             (neo-parser (caddr neo-code)))
     )
   )
 
