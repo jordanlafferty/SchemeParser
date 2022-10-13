d0a183fa936dbe68e542d930ee441791cd06f7de
jordanlafferty <78756047+jordanlafferty@users.noreply.github.com>
2022-10-13 11:42:36 -0500

    HW 13 - local environment support

--- Parser_Main.rkt
+++ Parser_Main.rkt
@@ -4,9 +4,14 @@
 (require "Parser_Parser.rkt")
 (require "Parser_Runner.rkt")
 
-(define env '((a 1) (b 2) (c 5)))
-(define sample-code '(call (function () (ask (bool != a b) (math - a b) (math - a b))) (a)))
-(display (neo-parser sample-code))
-(display(run-neo-parsed-code (neo-parser sample-code) env))
+;(define env '((a 1) (b 2) (c 5)))
+;(define sample-code '(call (function () (ask (bool != a b) (math - a b) (math - a b))) (a)))
+;(display (neo-parser sample-code))
+;(display(run-neo-parsed-code (neo-parser sample-code) env))
+
+(define parsed-neo-code (neo-parser '(call (function(a) (local-vars ((x 5) (y 6) (z 9)) (function (b)(math + a (math * b x))))) (2))))
+
+
+(run-neo-parsed-code parsed-neo-code env)
 
 (provide (all-defined-out))
