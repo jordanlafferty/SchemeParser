118041b24a49e92902f6b183316a3349d045cfb5
jordanlafferty <78756047+jordanlafferty@users.noreply.github.com>
2022-11-09 13:07:30 -0600

    Update Parser_Main.rkt

--- Parser_Main.rkt
+++ Parser_Main.rkt
@@ -5,16 +5,15 @@
 (require "Parser_Runner.rkt")
 
 (define env '((global (a 1) (b 2) (c 5))))
-(run-neo-parsed-code (neo-parser '(call (function (a) (call (function (r) a ) (a))) (5))) env)
+;(run-neo-parsed-code (neo-parser '(call (function (a) (call (function (r) a ) (a))) (5))) env)
 
-;(define env '((a 1) (b 2) (c 5)))
-;(define sample-code '(call (function () (ask (bool != a b) (math - a b) (math - a b))) (a)))
-;(display (neo-parser sample-code))
-;(display(run-neo-parsed-code (neo-parser sample-code) env))
+
+;(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
+(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
+(define parsed-neo-code (neo-parser sample-code))
+(run-neo-parsed-code parsed-neo-code env)
 
 ;(define parsed-neo-code (neo-parser '(call (function(a) (local-vars ((x 5) (y 6) (z 9)) (function (b)(math + a (math * b x))))) (2))))
 
 
 ;(run-neo-parsed-code parsed-neo-code env)
-
-(provide (all-defined-out))
