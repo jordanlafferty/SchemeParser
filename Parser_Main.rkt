66c79e081217a44d3a524a6c7dc058499a9cd022
jordanlafferty <78756047+jordanlafferty@users.noreply.github.com>
2022-11-20 11:36:59 -0600

    HW 16

--- Parser_Main.rkt
+++ Parser_Main.rkt
@@ -8,9 +8,10 @@
 ;(run-neo-parsed-code (neo-parser '(call (function (a) (call (function (r) a ) (a))) (5))) env)
 
 
-(define sample-code '(print a));
 ;(define sample-code2 '(assign a));
 ;(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
-(displayln (neo-parser sample-code))
+(define sample-code '(block (print a) (assign x 8) (assign y (math * x 2)) (print y) (assign z (math + b y)) (print z))) 
+
+;(displayln (neo-parser sample-code))
 (define parsed-neo-code (neo-parser sample-code))
 (run-neo-parsed-code parsed-neo-code env)
